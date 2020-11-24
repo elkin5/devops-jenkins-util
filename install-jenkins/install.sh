b931a15e37bf217f668bf2834fad118f7506eef1
Elkin Hurtado <syselkin5@gmail.com>
2020-11-23 22:05:39 -0500

    Agregar pasos a la instalacion de jenkins

--- install-jenkins/install.sh
+++ install-jenkins/install.sh
@@ -1,4 +1,19 @@
 # Depending on your system you might need to use sudo
+# 
+sudo apt update
+# Requiere de java
+sudo apt install openjdk-8-jdk wget
+# Agregue la clave del repositorio al sistema:
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+# Anexar la dirección del repositorio de paquetes de Debian a sources.list del servidor
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+# Usar docker desde jenkins
+sudo 
+sudo systemctl start jenkins
+
+
 apt-get update
 apt-get install openjdk-8-jdk wget gnupg
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
